c05e20622d638c8ba284c0556d0adb653c94cc6e
Alfonso Miguel Hernández <99462404+FonzHdz@users.noreply.github.com>
2024-11-10 16:52:44 -0500

    Update acceso doctor

--- obj/Release/net8.0/ProyectoInformatico.AssemblyInfo.cs
+++ obj/Release/net8.0/ProyectoInformatico.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoInformatico")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a644e24ade8e6832882a1fe4644c2ac38df1916")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoInformatico")]
